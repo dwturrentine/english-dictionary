abce9e930138c275b3f4305c0258f87fd820432c
Daryl <inbox.dwt@gmail.com>
2021-03-31 12:07:16 -0400

    Update settings.py

--- englishdictionary/englishdictionary/settings.py
+++ englishdictionary/englishdictionary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3g0zrzdidt+7p3&=$az06(4&5bqdd*fvnxe0h+oic^^h!kft35'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
